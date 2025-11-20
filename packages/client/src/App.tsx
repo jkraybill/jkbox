844876dde24646dbb7c55da40d0c5df7b2873d1b
Jeremy Kraybill <jkraybill@users.noreply.github.com>
2025-11-20 11:41:01 +1100

    Fix: Make root path (/) show Jumbotron directly
    Going to localhost:3000 now immediately shows the Jumbotron (title screen → lobby).
    Removed Home page from routing - TV is the primary interface.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
 import { Jumbotron } from './pages/Jumbotron'
 import { Join } from './pages/Join'
 import { Player } from './pages/Player'
@@ -7,8 +6,7 @@ import { Player } from './pages/Player'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/jumbotron" element={<Jumbotron />} />
+      <Route path="/" element={<Jumbotron />} />
       <Route path="/join/:roomId" element={<Join />} />
       <Route path="/play/:roomId" element={<Player />} />
     </Routes>
